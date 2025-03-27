617202f67f92da48ca4a020a30071b7ff291af8a
Sipho Nkebe <siphonkebe@gmail.com>
2025-03-27 13:27:06 +0200

    the safe commit, dont break it

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/bookings', bookingsRoutes);
-app.use('/users', userRouter);
+app.use('/user', userRouter);
 
 const PORT = process.env.PORT || 3000;
 
